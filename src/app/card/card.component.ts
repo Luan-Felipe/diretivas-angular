d49b54cb8721e2d583943eb7d94581bb130d6e30
Luan Felipe <luanfel2009@gmail.com>
2023-05-08 17:46:21 -0300

    Testes com diretivas

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+	produtos: string[] = [];
+	menuType: string = "chefe";
+	constructor() {
+		this.produtos = ['mouse', 'teclado', 'cabo', 'fonte'];
+	}
+
+	ngOnInit(): void {}
+
+	adicionar() {
+		this.produtos.push('Luan');
+	}
+
+	remover(index: number) {
+		this.produtos.splice(index, 1);
+	}
+}
